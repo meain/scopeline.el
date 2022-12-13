5829fbd2b94d4dffa07fecc73fa473663e1e0b2c
Abin Simon <mail@meain.io>
2022-12-13 22:23:16 +0530

    Add commentary and README

---
+++ README.md
@@ -0,0 +1,31 @@
+# context.el
+
+![Screenshot of Emacs frame with context.el enabled](https://user-images.githubusercontent.com/14259816/207393093-bef655ad-172c-48b3-aca5-cf20ee3a5894.png)
+
+This package lets you show the context of blocks like function
+definitions, loops, conditions etc. It does this by adding the
+first line of these blocks at the end of the last char of that
+block. It makes use of `tree-sitter' to figure out block start and
+end.
+
+The package exposes a single minor mode `context-mode' which you
+can use to enable or disable the functionality.
+
+Here is a sample `use-package' configuration:
+
+```emacs-lisp
+(use-package context
+  :after tree-sitter
+  :config (add-hook 'tree-sitter-mode-hook #'context-mode))
+```
+
+## Configuration
+
+- `context-overlay-prefix`: Change this to another string to change
+  the prefix. Defaults to <code>&nbsp;&nbsp;#&nbsp;</code>.
+- `context-face`: This is the face applied for the context overlay,
+  change this to change how it appears. Defaults to whatever comments
+  are highlighted with.
+- `context-targets`: Update this alist which maps `major-mode` to list
+  of tree-sitter entries to control what all blocks get context
+  overlays
--- context.el
+++ context.el
@@ -1,12 +1,43 @@
+;;; context.el --- Show context info of blocks in buffer -*- lexical-binding: t; -*-
+
+;; URL: https://github.com/meain/context.el
+;; Keywords: context, tree-sitter, convenience
+;; SPDX-License-Identifier: Apache-2.0
+;; Package-Requires: ((emacs "25.1") (tree-sitter "0.15.0"))
+;; Version: 0.1
+
+;;; Commentary:
+;; This package lets you show the context of blocks like function
+;; definitions, loops, conditions etc.  It does this by adding the
+;; first line of these blocks at the end of the last char of that
+;; block.  It makes use of `tree-sitter' to figure out block start and
+;; end.
+;;
+;; The package exposes a single minor mode `context-mode' which you
+;; can use to enable or disable the functionality.
+;;
+;; Here is a sample `use-package' configuration
+;;
+;; (use-package context
+;;   :after tree-sitter
+;;   :config (add-hook 'tree-sitter-mode-hook #'context-mode))
+;;
+;; You can find more info in the README for the project at
+;; https://github.com/meain/context.el
+
+;;; Code:
+
 (defvar context--overlays '())
 (defvar context-overlay-prefix "  # ")
-(defvar context-targets '((go-mode . ("function_declaration" "func_literal" "if_statement" "for_statement"))))
 (defface context-face
   '((default :inherit font-lock-comment-face))
   "Face for showing context info.")
+(defvar context-targets ;; TODO: Add more language modes
+  '((go-mode . ("function_declaration" "func_literal" "if_statement" "for_statement"))
+    (rust-mode . ("function_item" "for_expression" "if_expression"))))
 
 (defun context--add-overlay (pos text)
-  "Adds overlay at `POS' with the specified `TEXT'."
+  "Add overlay at `POS' with the specified `TEXT'."
   (let ((ov (make-overlay pos pos)))
     (overlay-put ov 'after-string
                  (propertize (format "%s%s" context-overlay-prefix text)
@@ -44,7 +75,7 @@
              matches)))
 
 (defun context--redisplay (&rest _)
-  "Redisplay all the context entries."
+  "Re-display all the context entries."
   (if context-mode
       (progn
         (context--delete-all-overlays)
